ba39040f6425a60947f31e3418bb6a5c700b4eca
Nitin Bakade <mylonelyplacealone@gmail.com>
2019-03-20 23:44:11 +0530

    SOrtin added in oneview

--- src/app/mutualfund/mfoneview/mfoneview.component.ts
+++ src/app/mutualfund/mfoneview/mfoneview.component.ts
@@ -14,6 +14,8 @@ export class MfoneviewComponent implements OnInit {
   recordsSubscription = new Subscription();
   totalcost:number = 0;
   totalvalue:number = 0;
+  toggle:boolean = true;
+  sortcolumn:string;
 
   constructor(private mfService:MfService) { }
 
@@ -37,11 +39,75 @@ export class MfoneviewComponent implements OnInit {
 
       this.totalcost = this.groupedRecords.reduce((sum, item) => sum + (item.purchasenav * item.units), 0)
       this.totalvalue = this.groupedRecords.reduce((sum, item) => sum + (item.currentnav * item.units), 0)
-      this.groupedRecords.sort((a,b)=> a.name.localeCompare(b.name));
+
+      this.groupedRecords.sort((a,b)=> b.name.localeCompare(a.name));
+      // this.groupedRecords.sort((a,b)=> (((a.currentnav - a.purchasenav) * a.units)) - ((b.currentnav - b.purchasenav) * b.units));
       //console.log(this.groupedRecords);
     });
 
     this.mfService.GetMFList();
+  }
+
+  sortData(value){
+    this.sortcolumn = value;
+    switch(value){
+      case "name":{//name
+        this.toggle ? this.groupedRecords.sort((a,b)=> a.name.localeCompare(b.name)) : this.groupedRecords.sort((a,b)=> b.name.localeCompare(a.name));
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      case "units":
+      case "purchasenav":
+      case "currentnav":
+      {//units
+        // this.toggle ? this.groupedRecords.sort((a,b)=> a.units - b.units) : this.groupedRecords.sort((a,b)=> b.units - a.units);
+        this.toggle ? this.groupedRecords.sort((a,b)=> a[value] - b[value]) : this.groupedRecords.sort((a,b)=> b[value] - a[value]);
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      case "costvalue":{//Cost Value
+        this.toggle ? 
+          this.groupedRecords.sort((a,b)=> (a.purchasenav * a.units) - (b.purchasenav * b.units)) :
+          this.groupedRecords.sort((a,b)=>(b.purchasenav * b.units) - (a.purchasenav * a.units));
+        
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      case "currentvalue":{//CUrrent Value
+        this.toggle ? 
+          this.groupedRecords.sort((a,b)=> (a.currentnav * a.units) - (b.currentnav * b.units)) :
+          this.groupedRecords.sort((a,b)=>(b.currentnav * b.units) - (a.currentnav * a.units));
+        
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      case "profit":{//profit amount
+        this.toggle ? 
+          this.groupedRecords.sort((a,b)=> (((a.currentnav - a.purchasenav) * a.units)) - ((b.currentnav - b.purchasenav) * b.units)) :
+          this.groupedRecords.sort((a,b)=> (((b.currentnav - b.purchasenav) * b.units)) - ((a.currentnav - a.purchasenav) * a.units));
+        
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      case "percentage":{//profit percentage
+        this.toggle ? 
+          this.groupedRecords.sort((a,b)=> ((((a.currentnav/a.purchasenav)-1) * 100) - (((b.currentnav/b.purchasenav)-1) * 100))):
+          this.groupedRecords.sort((a,b)=> ((((b.currentnav/b.purchasenav)-1) * 100) - (((a.currentnav/a.purchasenav)-1) * 100)));
+        
+        this.toggle = !this.toggle;
+        break;
+      }
+
+      default: { 
+        this.groupedRecords.sort((a,b)=> a.name.localeCompare(b.name));
+        break; 
+     } 
+    }
   }
 
   objectKey(obj) {
