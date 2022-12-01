5c4de4a565d4bb51c9e00695ab08c1e24e7c0bbf
mylonelyplacealone <43513991+mylonelyplacealone@users.noreply.github.com>
2022-12-01 10:06:54 +0530

    Render link updated

--- src/app/common/config.service.ts
+++ src/app/common/config.service.ts
@@ -5,7 +5,7 @@ export class ConfigClass{
   public static mfGetDataUrl:string = "https://api.mfapi.in/mf/";//"http://localhost:8010/proxy/mf/"
   public static mfSearchListURL:string = "https://api.mfapi.in/mf/search?q=";//"http://localhost:8010/proxy/mf/search?q="
   //https://www.mfapi.in/
-  public static restAPIURL:string = "https://mfportalrestapi.herokuapp.com/api/";
+  public static restAPIURL:string = "https://mfportalrestapi.onrender.com/api/";
   // public static restAPIURL:string = "http://localhost:5000/api/";
 
   //Stoc URL Below
